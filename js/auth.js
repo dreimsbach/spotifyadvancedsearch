a2a4ca44f9129a2963b029a503b9ff9e62dfb21d
Dirk Reimsbach <Dirk@Dirks-MBP.fritz.box>
2017-06-03 06:52:13 +0200

    Merge branch spotifylabelnews to master

---
+++ js/auth.js
@@ -0,0 +1,71 @@
+// Sportify OAuth
+var AUTH = (function() {
+
+  var accessToken = '';
+
+  var login = function(successCallback) {
+    var CLIENT_ID = '66ed85f628e24aef9b803c8b8cca4de9';
+
+    if (location.host === 'localhost') {
+      REDIRECT_URI = 'http://localhost/spotifysearch/callback.html';
+    } else {
+      REDIRECT_URI = 'http://www.monoaural.de/callback.html';
+    }
+
+    function getLoginURL() {
+      return 'https://accounts.spotify.com/authorize?client_id=' +
+        CLIENT_ID +
+        '&redirect_uri=' + encodeURIComponent(REDIRECT_URI) +
+        '&response_type=token';
+    }
+
+    window.addEventListener("message", function(event) {
+      var hash = JSON.parse(event.data);
+      if (hash.type == 'access_token') {
+        setAccessToken(hash.access_token, hash.expires_in || 3600);
+        if (successCallback) {
+          successCallback();
+        }
+      }
+    }, false);
+
+    var width = 450,
+      height = 730,
+      left = (screen.width / 2) - (width / 2),
+      top = (screen.height / 2) - (height / 2);
+
+    w = window.open(getLoginURL(),
+      'Spotify',
+      'menubar=no,location=no,resizable=no,scrollbars=no,status=no, width=' +
+      width + ', height=' + height + ', top=' + top + ', left=' + left
+    );
+  };
+
+  var getAccessToken = function() {
+    var expires = localStorage.getItem('pa_expires', '0');
+    if ((new Date()).getTime() > expires) {
+      return '';
+    }
+    var token = localStorage.getItem('pa_token', '');
+    return token;
+  };
+
+  var setAccessToken = function(token, expires_in) {
+    var t = new Date();
+    t.setSeconds(t.getSeconds() + expires_in - 1);
+
+    localStorage.setItem('pa_token', token);
+    localStorage.setItem('pa_expires', t.getTime());
+  };
+
+  var isLoggedin = function() {
+    return getAccessToken() !== '';
+  }
+
+  return {
+    login: login,
+    isLoggedin: isLoggedin,
+    getAccessToken: getAccessToken
+  }
+
+})();
--- js/base.js
+++ js/base.js
@@ -1,237 +1,305 @@
 // find template and compile it
-var templateSource = document.getElementById('results-template').innerHTML,
-    template = Handlebars.compile(templateSource),
-    noResultTemplateSource = document.getElementById('no-results-template').innerHTML,
-    noResultTemplate = Handlebars.compile(noResultTemplateSource),
-    resultsPlaceholder = document.getElementById('results'),
-    playingCssClass = 'playing',
-    audioObject = null;
+var
+  templateSource = document.getElementById('results-template').innerHTML,
+  template = Handlebars.compile(templateSource),
+  noResultTemplateSource = document.getElementById('no-results-template').innerHTML,
+  noResultTemplate = Handlebars.compile(noResultTemplateSource),
+  resultsPlaceholder = document.getElementById('results'),
+  definiteResult = [];
 
-var fetchTracks = function (albumId, callback) {
-    $.ajax({
-        url: 'https://api.spotify.com/v1/albums/' + albumId,
-        success: function (response) {
-            //console.log(response);
-            callback(response);
-        }
-    });
-};
-
-var pauseAudio = function () {
-    if (audioObject) {
-        audioObject.pause();
+var fetchTracks = function(albumId, callback) {
+  $.ajax({
+    url: 'https://api.spotify.com/v1/albums/' + albumId,
+    headers: {
+      'Authorization': 'Bearer ' + AUTH.getAccessToken()
+    },
+    success: function(response) {
+      callback(response);
     }
+  });
 };
 
-var searchAlbums = function (query, callback) {
-    $.ajax({
-        url: 'https://api.spotify.com/v1/search',
-        data: {
-            q: query,
-            type: 'album',
-            market: 'DE'
-        },
-        success: function (response) {
-            //console.log(response);
-            var list = [],
-                newReleaseFlagDate = new Date(),
-              records = response.albums.items;
-
-
-            if (response.albums.items.length == 0) {
-                callback(list);
-            }
-            // New Releases = within two weeks
-            newReleaseFlagDate.setDate(newReleaseFlagDate.getDate() - 14);
-
-            for (item in records) {
-                var record = records[item],
-                  entry = {};
-
-                entry.uri = record.uri;
-                entry.id = record.id;
-                entry.converimage = record.images[0].url;
-                entry.albumType = record.album_type;
-
-                (function(record, entry, list, newReleaseFlagDate){
-                    fetchTracks(record.id, function(data) {
-                        entry.artist = data.artists[0].name;
-                        entry.album = record.name;
-                        entry.releaseDate = data.release_date;
-                        entry.newFlag = new Date(entry.releaseDate) >= newReleaseFlagDate;
-                        entry.genres = data.gernres;
-                        list.push(entry);
-                        // Ende rausfinden
-                        if (records.length == list.length) {
-                            list.sort(function (a, b) {
-                              if (a.releaseDate > b.releaseDate) {
-                                return -1;
-                              }
-                              if (a.releaseDate < b.releaseDate) {
-                                return 1;
-                              }
-                              // a must be equal to b
-                              return 0;
-                            });
-                            callback(list);
-                      }
-                    })
-                })(record, entry, list, newReleaseFlagDate);
-            }
-        }
-    });
-};
-
-var audioPlayerInit =  function (e) {
-    var target = e.target;
-    if (target !== null && target.classList.contains('cover')) {
-        if (target.classList.contains(playingCssClass)) {
-            pauseAudio();
-        } else {
-            pauseAudio();
-            fetchTracks(target.getAttribute('data-album-id'), function (data) {
-                audioObject = new Audio(data.tracks.items[0].preview_url);
-                audioObject.play();
-                target.classList.add(playingCssClass);
-                audioObject.addEventListener('ended', function () {
-                    target.classList.remove(playingCssClass);
-                });
-                audioObject.addEventListener('pause', function () {
-                    target.classList.remove(playingCssClass);
-                });
-            });
-        }
-    }
-};
-
-var openSpotifyURL =  function (e) {
-    var target = e.target;
-    if (target !== null && target.classList.contains('cover')) {
-        var url = target.getAttribute('data-album-url');
-        document.location = url;
-    }
-};
-
-//results.addEventListener('mousedown', audioPlayerInit);
-results.addEventListener('click', openSpotifyURL);
-
-
-window.addEventListener("keypress", function(e) {
-  if (e.keyCode === 0 || e.keyCode === 32) {
-    //e.preventDefault();
-  pauseAudio();
+var searchAlbums = function(requestObj, callback, year) {
+  var yearAppendix = "tag:new";
+  if (year) {
+    yearAppendix = "year:" + year;
   }
-});
 
-window.addEventListener('focus', function() {
-  pauseAudio();
-});
-
-window.addEventListener('blur', function() {
-  pauseAudio();
-});
-
-document.getElementById('search-form').addEventListener('submit', function (e) {
-    e.preventDefault();
-
-    var query,
-      artist,
-        year,
-        label,
-      artistField = document.getElementById('artist').value,
-        yearField = document.getElementById('year').value,
-        labelField = document.getElementById('label').value;
-
-    if (artistField) {
-        artist = "artist:\""  + artistField + "\"";
-    }
-
-    if (yearField) {
-        if ("new" === yearField) {
-            year = "tag:new";
-        } else {
-          year = "year:"  + yearField ;
-        }
-    }
-
-    if (labelField) {
-        label = "label:\""  + labelField + "\"";
-    }
+  $.ajax({
+    url: 'https://api.spotify.com/v1/search',
+    headers: {
+      'Authorization': 'Bearer ' + AUTH.getAccessToken()
+    },
+    data: {
+      q: "label:\"" + requestObj.label + "\", " + yearAppendix,
+      type: 'album',
+      market: 'DE'
+    },
+    success: function(response) {
+      var list = [],
+        newReleaseFlagDate = new Date(),
+        records = response.albums.items;
 
 
-
-    query = $.grep([artist, label, year], Boolean).join(", ");
-
-    searchAlbums(query, function(data) {
-        if (data.length > 0) {
-            resultsPlaceholder.innerHTML = template(data);
-            addToSearchHistory(labelField, yearField, artistField);
-        } else {
-            resultsPlaceholder.innerHTML = noResultTemplate(data);
-        }
-        showLastSearches();
-    });
-
-
-}, false);
-
-var addToSearchHistory = function (label, year, artist) {
-    var lastSearches = JSON.parse(localStorage.getItem('lastSearchArray')) || {},
-      lastSearchObj = {
-            "label": label,
-            "year": year,
-            "artist": artist
-        },
-        lastSearchKey = encodeURI(label+year+artist);
-
-
-    lastSearches[lastSearchKey] =  lastSearchObj;
-    localStorage.setItem("lastSearchArray", JSON.stringify(lastSearches));
-}
-
-var showLastSearches = function () {
-    var $lastSearchesContainer = $("#lastSearchesContainer"),
-      $lastSearchesList = $("#lastSearches");
-    $lastSearchesList.empty();
-
-    var lastSearches = JSON.parse(localStorage.getItem('lastSearchArray')) || {};
-
-    if (!$.isEmptyObject(lastSearches)) {
-        $lastSearchesContainer.show();
-
-        for (key in lastSearches) {
-            var entry = lastSearches[key];
-
-            $('<li/>', {
-                'data-label': entry.label,
-                'data-year': entry.year,
-                'data-artist': entry.artist
-            }) //
-            .append("<span>Label:  "+ entry.label +"</span>") //
-            .append("<span>Year:  "+ entry.year +"</span>") //
-            .append("<span>Artist:  "+ entry.artist +"</span>") //
-            .addClass("last-search-entry") //
-            .appendTo($lastSearchesList) //
-            .click(function() {
-                var $element = $(this);
-                $("#label").val($element.data("label"));
-                $("#year").val($element.data("year"));
-                $("#artist").val($element.data("artist"));
-                $("#search").click();
-            });
+      if (response.albums.items.length == 0) {
+        callback(list);
       }
-    } else {
-        $lastSearchesContainer.hide();
+      // New Releases = within two weeks
+      newReleaseFlagDate.setDate(newReleaseFlagDate.getDate() - 14);
+
+      for (item in records) {
+        var record = records[item],
+          entry = {};
+
+        entry.uri = record.uri;
+        entry.id = record.id;
+        entry.coverimage = record.images[0].url;
+        entry.albumType = record.album_type;
+        entry.label = requestObj.label;
+
+        (function(record, entry, list, newReleaseFlagDate) {
+          fetchTracks(record.id, function(data) {
+            entry.artist = data.artists[0].name;
+            entry.album = record.name;
+            entry.releaseDate = data.release_date;
+            entry.newFlag = new Date(entry.releaseDate) >=
+              newReleaseFlagDate;
+            entry.genres = data.gernres;
+            list.push(entry);
+            if (records.length == list.length) {
+              callback(list);
+            }
+          })
+        })(record, entry, list, newReleaseFlagDate);
+      }
     }
+  });
+};
+
+var openSpotifyURL = function(e) {
+  var target = e.target;
+  if (target !== null && target.classList.contains('cover')) {
+    var url = target.getAttribute('data-album-url');
+    document.location = url;
+  }
+};
+
+var searchLabels = function() {
+  document.getElementById('results').innerHTML = "";
+  definiteResult = [];
+
+  var
+    lastSearches = getLabelsFromStorage(),
+    totalSize = Object.keys(lastSearches).length,
+    count = 0;
+
+  if (totalSize > 0) {
+    for (key in lastSearches) {
+      var entry = lastSearches[key];
+      count++;
+      (function(entry, totalSize) {
+        searchAlbums(entry, function(data) {
+          if (data.length > 0) {
+            definiteResult = definiteResult.concat(data);
+          }
+          if (count == totalSize) {
+            definiteResult.sort(function(a, b) {
+              if (a.releaseDate > b.releaseDate) {
+                return -1;
+              }
+              if (a.releaseDate < b.releaseDate) {
+                return 1;
+              }
+              return 0;
+            });
+            document.getElementById('results').innerHTML = template(
+              definiteResult);
+          }
+        });
+      })(entry, totalSize);
+    }
+  }
+
 }
 
-var clearHistory = function () {
-    localStorage.clear();
-    showLastSearches();
+var addToLabelList = function(label) {
+  if (label && !label.startsWith("#")) {
+    var currentLabels = getLabelsFromStorage(),
+      labelObj = {
+        "label": label
+      },
+      labelKey = encodeURI(label);
+
+    currentLabels[labelKey] = labelObj;
+    setLabelsToStorage(currentLabels);
+  }
 }
 
-$(document).ready(function() {
-    $("#label").focus();
-    $("#clearHistory").click(clearHistory);
-    showLastSearches();
-});
+var showLabelList = function() {
+  var $labelsContainer = $("#labels-container"),
+    $labelsList = $("#last-searches");
+  $labelsList.empty();
+
+  var lastSearches = getLabelsFromStorage();
+
+  if (!$.isEmptyObject(lastSearches)) {
+    $labelsContainer.show();
+
+    for (key in lastSearches) {
+      var entry = lastSearches[key];
+
+      $('<li/>', {
+          'data-label': entry.label
+        }) //
+        .append("<span>" + entry.label + "</span>") //
+        .addClass("last-search-entry") //
+        .appendTo($labelsList) //
+        .click(function() {
+          var $element = $(this);
+          removeLabel($element.data("label"));
+          showLabelList();
+        });
+    }
+  } else {
+    $labelsContainer.hide();
+  }
+}
+
+var removeLabel = function(labelToDelete) {
+  var newList = {},
+    oldList = getLabelsFromStorage();
+
+  if (!$.isEmptyObject(oldList)) {
+    for (key in oldList) {
+      var entry = oldList[key];
+      if (labelToDelete !== entry.label) {
+        newList[key] = entry;
+      }
+    }
+  }
+  setLabelsToStorage(newList);
+
+  searchLabels();
+}
+
+var getLabelsFromStorage = function() {
+  return JSON.parse(localStorage.getItem('labelList')) || {};
+}
+
+var setLabelsToStorage = function(currentLabels) {
+  localStorage.setItem("labelList", JSON.stringify(currentLabels));
+}
+
+var clearHistory = function() {
+  localStorage.clear();
+  showLabelList();
+}
+
+var submitSearchForm = function(e) {
+  e.preventDefault();
+
+  var labelField = document.getElementById('label').value;
+  if (labelField) {
+    addToLabelList(labelField);
+    showLabelList();
+  }
+
+  searchLabels();
+}
+
+var useDataFromFile = function(data) {
+  localStorage.clear();
+
+  var lines = data.split('\n');
+  for (key in lines) {
+    var entry = lines[key];
+    addToLabelList(entry);
+  }
+
+  showLabelList();
+  searchLabels();
+}
+
+var changeOrUploadFile = function(e) {
+  e.preventDefault();
+
+  var
+    selectedFile = document.getElementById('label-file').files[0],
+    reader = new FileReader();
+
+  reader.onload = function() {
+    useDataFromFile(reader.result);
+  };
+  reader.readAsText(selectedFile);
+
+  $("#file-upload-container").hide();
+}
+
+var readFromURL = function(e) {
+  e.preventDefault();
+  $.ajax({
+    url: $('#label-url-list').val(),
+    success: useDataFromFile
+  });
+  $("#url-upload-container").hide();
+}
+
+var searchDirectLabel = function(e) {
+  e.preventDefault();
+
+  var label = document.getElementById('label').value;
+  if (!label) {
+    return false;
+  }
+
+  searchAlbums({
+    "label": label
+  }, function(data) {
+    document.getElementById('results').innerHTML = template(
+      data);
+  }, document.getElementById('year').value);
+}
+
+var initLogin = function() {
+  if (AUTH.isLoggedin()) {
+    //searchLabels();
+  } else {
+    $("#logged-in-content-container").hide();
+    $("#login-button-container").show();
+    $("#btn-login").click(function() {
+      AUTH.login(function() {
+        $("#login-button-container").hide();
+        $("#logged-in-content-container").show();
+        //searchLabels();
+      })
+    });
+  }
+}
+
+var init = function() {
+  $("#label").focus();
+  $("#clear-history").on("click", clearHistory);
+  $("#find-records").on("click", searchLabels);
+  $("#add-to-list-btn").on("click", submitSearchForm);
+  $("#search-form").on("submit", searchDirectLabel);
+  $("#label-file").on("change", changeOrUploadFile);
+  $("#url-update-form").on("submit", readFromURL);
+
+  $("#upload-from-list-btn").on("click", function() {
+    $("#url-upload-container").hide();
+    $("#file-upload-container").show();
+  });
+
+  $("#upload-from-url-btn").on("click", function() {
+    $("#file-upload-container").hide();
+    $("#url-upload-container").show();
+  });
+  showLabelList();
+  initLogin();
+
+}
+
+// INIT
+$(document).ready(init);
+results.addEventListener('click',
+  openSpotifyURL);
