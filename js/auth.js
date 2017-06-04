a2fa966480864b1c6472c25561ce3085b06fa33c
Dirk Reimsbach <Dirk@Dirks-MBP.fritz.box>
2017-06-04 07:52:10 +0200

    https

--- js/auth.js
+++ js/auth.js
@@ -9,7 +9,7 @@ var AUTH = (function() {
     if (location.host === 'localhost') {
       REDIRECT_URI = 'http://localhost/spotifysearch/callback.html';
     } else {
-      REDIRECT_URI = 'http://www.monoaural.de/callback.html';
+      REDIRECT_URI = 'https://www.monoaural.de/callback.html';
     }
 
     function getLoginURL() {
